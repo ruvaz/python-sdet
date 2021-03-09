66e3f851d6fc68944c88d94c59349d3e9aae7b48
Ruben Vazquez <ruvaz@live.com>
2021-03-08 23:53:07 -0600

    read write files

---
+++ pythonBasic/writefile.py
@@ -0,0 +1,17 @@
+# file = open("text.txt")
+#
+# file.close()
+
+# abr ele archivo y lo cierra cuando ya no le ocupa es mejor que el flo.eclose()
+
+# read the file and store all the lines in list
+#reverse the list
+with open('text.txt','r') as reader:
+    lines = reader.readlines()
+    print(lines)
+    lines.reverse()
+    print(lines)
+    with open('text.txt','w') as writer:
+        writer.write(lines)
+
+
